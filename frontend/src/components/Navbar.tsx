6955e121ffd8d7a6680865334e39839094b928e4
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-11-27 19:02:34 -0500

    wip: navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
+const navbarConfig = [
+  {
+    name: "About",
+    href: "/",
+  },
+];
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-center w-full p-4">
+      {navbarConfig.map((item) => (
+        <a
+          className="font-bold text-xl"
+          key={`${item.href}-${item.name}`}
+          href={item.href}
+        >
+          {item.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
