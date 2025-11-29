14fdd3013771cf9d792444ba2b7e29ae2499b386
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-11-29 15:36:00 -0500

    add time-based revalidation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,9 @@ import Hero from "@/components/Hero";
 import Experience from "@/components/Experience";
 import { fetchHero, fetchExperience } from "@/utils/fetch-hompage";
 
+// https://nextjs.org/docs/app/guides/incremental-static-regeneration#time-based-revalidation
+export const revalidate = 3600; // Revalidate every hour
+
 export default async function Home() {
   const [heroData, experienceData] = await Promise.all([
     fetchHero(),
