e80b9e745d66bd2bead309fd51d3668dd0027d40
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-11-28 21:57:27 -0500

    add typing text

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
 import { RichText } from "@payloadcms/richtext-lexical/react";
 import Image from "next/image";
 import ErrorMsg from "./ErrorMsg";
+import TypingText from "./ui/shadcn-io/typing-text";
 
 interface HeroProps {
   data: SerializedEditorState | null;
@@ -14,7 +15,23 @@ export default function Hero({ data }: HeroProps) {
       className="py-4 flex flex-col md:flex-row justify-between gap-2"
       id="about"
     >
-      <div className="w-full mb-4 md:mb-0 flex items-center">
+      <div className="w-full mb-4 md:mb-0 flex justify-center flex-col">
+        <TypingText
+          text={[
+            "Hi, I'm Brandon.",
+            "Let's build something beautiful together :)",
+          ]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+          className="font-bold"
+          textColors={["black"]}
+          variableSpeed={{ min: 50, max: 120 }}
+        />
+        <p>
+          <br />
+        </p>
         {data ? <RichText data={data} /> : <ErrorMsg />}
       </div>
       <div className="flex items-center justify-center">
