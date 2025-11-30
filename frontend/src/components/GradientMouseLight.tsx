31efcff31ad6135d1497d767ee6ead00df497430
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-11-30 17:51:21 -0500

    wip: mouse animation

--- frontend/src/components/GradientMouseLight.tsx
+++ frontend/src/components/GradientMouseLight.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useIsMobile } from "@/hooks/use-mobile";
 import * as motion from "motion/react-client";
 
-const CONTAINER_SIZE = 700;
+const CONTAINER_SIZE = 600;
 const SMALL_SQUARE_SIZE = 20;
-const NUM_SQUARES = 60;
 
 const getRandomPosition = () => ({
   x: Math.random() * (CONTAINER_SIZE - SMALL_SQUARE_SIZE),
@@ -13,29 +12,48 @@ const getRandomPosition = () => ({
 });
 
 export default function GradientMouseLight() {
+  const squareId = useRef(0);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [squares, setSquares] = useState(() =>
-    Array.from({ length: NUM_SQUARES }, (_, i) => ({
-      id: i,
-      ...getRandomPosition(),
-    }))
-  );
+  const [squares, setSquares] = useState<
+    Array<{
+      id: number;
+      x: number;
+      y: number;
+      centerX: number;
+      centerY: number;
+    }>
+  >([]);
   const isMobile = useIsMobile();
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      setMousePosition({
-        x: event.clientX + window.scrollX,
-        y: event.clientY + window.scrollY,
-      });
+      const x = event.clientX + window.scrollX;
+      const y = event.clientY + window.scrollY;
+      setMousePosition({ x, y });
     };
+
+    const interval = setInterval(() => {
+      setSquares((prev) => [
+        ...prev,
+        {
+          id: squareId.current++,
+          ...getRandomPosition(),
+          centerX: mousePosition.x,
+          centerY: mousePosition.y,
+        },
+      ]);
+    }, 200);
+
     document.addEventListener("mousemove", handleMouseMove);
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
+      clearInterval(interval);
     };
-  }, []);
+  }, [mousePosition]);
 
   if (isMobile) return null;
+
+  console.log("test");
   return (
     <div
       className="fixed inset-0 -z-10"
@@ -43,42 +61,29 @@ export default function GradientMouseLight() {
         background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, #e7e7e7, white 25%)`,
       }}
     >
-      <div
-        className="absolute rounded-full overflow-hidden"
-        style={{
-          //   background: "red",
-          width: CONTAINER_SIZE,
-          height: CONTAINER_SIZE,
-          left: mousePosition.x - CONTAINER_SIZE / 2,
-          top: mousePosition.y - CONTAINER_SIZE / 2,
-          zIndex: -1,
-        }}
-      >
-        {squares.map((square, index) => (
-          <motion.div
-            key={`${square.id}-${square.x}-${square.y}`}
-            className="absolute bg-gray-400"
-            style={{
-              width: SMALL_SQUARE_SIZE,
-              left: square.x,
-              top: square.y + SMALL_SQUARE_SIZE / 2,
-            }}
-            initial={{ height: 0 }}
-            animate={{ height: [0, SMALL_SQUARE_SIZE, 0] }}
-            transition={{
-              duration: 2,
-              delay: index * 0.2,
-            }}
-            onAnimationComplete={() => {
-              setSquares((prev) =>
-                prev.map((s) =>
-                  s.id === square.id ? { ...s, ...getRandomPosition() } : s
-                )
-              );
-            }}
-          />
-        ))}
-      </div>
+      {squares.map((square) => (
+        <motion.div
+          key={square.id}
+          className="absolute bg-gray-400"
+          style={{
+            width: SMALL_SQUARE_SIZE,
+            left: square.centerX - CONTAINER_SIZE / 2 + square.x,
+            top:
+              square.centerY -
+              CONTAINER_SIZE / 2 +
+              square.y +
+              SMALL_SQUARE_SIZE / 2,
+          }}
+          initial={{ height: 0 }}
+          animate={{ height: [0, SMALL_SQUARE_SIZE, 0] }}
+          transition={{
+            duration: 1,
+          }}
+          onAnimationComplete={() => {
+            setSquares((prev) => prev.filter((s) => s.id !== square.id));
+          }}
+        />
+      ))}
     </div>
   );
 }
