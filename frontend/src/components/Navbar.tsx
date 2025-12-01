68d0eb8320d9a4695aceb41f45c84108b9fe0fb9
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-11-30 23:44:55 -0500

    wip: light-dark mode

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -20,6 +20,7 @@ import {
 } from "@/components/ui/sheet";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Button } from "@/components/ui/button";
+import { LucideSun, LucideMoon } from "lucide-react";
 
 const navItems = [
   { href: "/#about", label: "About" },
@@ -28,6 +29,9 @@ const navItems = [
 ];
 
 export default function Navbar() {
+  const [themeMode, setThemeMode] = useState(
+    typeof window !== "undefined" && localStorage.getItem("themeMode")
+  );
   const isMobile = useIsMobile();
   const [open, setOpen] = useState(false);
   const [activeSection, setActiveSection] = useState(
@@ -64,114 +68,141 @@ export default function Navbar() {
           <h1 className="text-base">Brandon Cheung</h1>
           <h2 className="text-sm">Software Developer</h2>
         </Link>
-        {isMobile ? (
-          <Sheet open={open} onOpenChange={setOpen}>
-            <SheetTrigger asChild>
-              <Button variant="default" size="icon">
-                <HamburgerIcon className="h-5 w-5" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="right">
-              <VisuallyHidden>
-                <SheetTitle>Navigation Menu</SheetTitle>
-                <SheetDescription>Site navigation links</SheetDescription>
-              </VisuallyHidden>
-              <NavigationMenuList className="flex flex-col gap-4 mt-8 px-4 py-2">
-                {navItems.map((item) => {
-                  const handleClick = (
-                    e: React.MouseEvent<HTMLAnchorElement>
-                  ) => {
-                    e.preventDefault();
-                    setOpen(false);
-                    const id = item.href.split("#")[1];
-                    setActiveSection(id);
-                    setTimeout(() => {
-                      const element = document.getElementById(id);
-                      if (element) {
-                        const offset = 100;
-                        const elementPosition =
-                          element.getBoundingClientRect().top;
-                        const offsetPosition =
-                          elementPosition + window.scrollY - offset;
-                        window.scrollTo({
-                          top: offsetPosition,
-                          behavior: "smooth",
-                        });
-                      }
-                    }, 300);
-                  };
-                  if (item.href.includes("#")) {
+        <div className="flex items-center gap-2">
+          {isMobile ? (
+            <Sheet open={open} onOpenChange={setOpen}>
+              <SheetTrigger asChild>
+                <Button variant="default" size="icon">
+                  <HamburgerIcon aria-label="menu" className="h-5 w-5" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="right">
+                <VisuallyHidden>
+                  <SheetTitle>Navigation Menu</SheetTitle>
+                  <SheetDescription>Site navigation links</SheetDescription>
+                </VisuallyHidden>
+                <NavigationMenuList className="flex flex-col gap-4 mt-8 px-4 py-2">
+                  {navItems.map((item) => {
+                    const handleClick = (
+                      e: React.MouseEvent<HTMLAnchorElement>
+                    ) => {
+                      e.preventDefault();
+                      setOpen(false);
+                      const id = item.href.split("#")[1];
+                      setActiveSection(id);
+                      setTimeout(() => {
+                        const element = document.getElementById(id);
+                        if (element) {
+                          const offset = 100;
+                          const elementPosition =
+                            element.getBoundingClientRect().top;
+                          const offsetPosition =
+                            elementPosition + window.scrollY - offset;
+                          window.scrollTo({
+                            top: offsetPosition,
+                            behavior: "smooth",
+                          });
+                        }
+                      }, 300);
+                    };
+                    if (item.href.includes("#")) {
+                      return (
+                        <a
+                          onClick={handleClick}
+                          key={item.href}
+                          href={item.href}
+                          className="text-lg"
+                        >
+                          {item.label}
+                        </a>
+                      );
+                    }
+
                     return (
-                      <a
+                      <Link
                         onClick={handleClick}
                         key={item.href}
                         href={item.href}
                         className="text-lg"
                       >
                         {item.label}
-                      </a>
-                    );
-                  }
-
-                  return (
-                    <Link
-                      onClick={handleClick}
-                      key={item.href}
-                      href={item.href}
-                      className="text-lg"
-                    >
-                      {item.label}
-                    </Link>
-                  );
-                })}
-              </NavigationMenuList>
-            </SheetContent>
-          </Sheet>
-        ) : (
-          <NavigationMenuList>
-            {navItems.map((item) => {
-              const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-                e.preventDefault();
-                const id = item.href.split("#")[1];
-
-                setActiveSection(id);
-                setTimeout(() => {
-                  const element = document.getElementById(id);
-                  if (element) {
-                    const offset = 100;
-                    const elementPosition = element.getBoundingClientRect().top;
-                    const offsetPosition =
-                      elementPosition + window.scrollY - offset;
-                    window.scrollTo({
-                      top: offsetPosition,
-                      behavior: "smooth",
-                    });
-                  }
-                }, 300);
-              };
-
-              return (
-                <NavigationMenuItem key={item.href}>
-                  <NavigationMenuLink
-                    className={navigationMenuTriggerStyle()}
-                    isActive={activeSection === item.href.split("#")[1]}
-                    asChild
-                  >
-                    {item.href.includes("#") ? (
-                      <a href={item.href} onClick={handleClick}>
-                        {item.label}
-                      </a>
-                    ) : (
-                      <Link href={item.href} onClick={handleClick}>
-                        {item.label}
                       </Link>
-                    )}
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              );
-            })}
-          </NavigationMenuList>
-        )}
+                    );
+                  })}
+                </NavigationMenuList>
+              </SheetContent>
+            </Sheet>
+          ) : (
+            <NavigationMenuList>
+              {navItems.map((item) => {
+                const handleClick = (
+                  e: React.MouseEvent<HTMLAnchorElement>
+                ) => {
+                  e.preventDefault();
+                  const id = item.href.split("#")[1];
+
+                  setActiveSection(id);
+                  setTimeout(() => {
+                    const element = document.getElementById(id);
+                    if (element) {
+                      const offset = 100;
+                      const elementPosition =
+                        element.getBoundingClientRect().top;
+                      const offsetPosition =
+                        elementPosition + window.scrollY - offset;
+                      window.scrollTo({
+                        top: offsetPosition,
+                        behavior: "smooth",
+                      });
+                    }
+                  }, 300);
+                };
+
+                return (
+                  <NavigationMenuItem key={item.href}>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                      isActive={activeSection === item.href.split("#")[1]}
+                      asChild
+                    >
+                      {item.href.includes("#") ? (
+                        <a href={item.href} onClick={handleClick}>
+                          {item.label}
+                        </a>
+                      ) : (
+                        <Link href={item.href} onClick={handleClick}>
+                          {item.label}
+                        </Link>
+                      )}
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                );
+              })}
+            </NavigationMenuList>
+          )}
+          <Button
+            size="icon"
+            onClick={() => {
+              const html = document.querySelector("html");
+              if (themeMode === "dark") {
+                html?.classList.remove("dark");
+                localStorage.setItem("themeMode", "light");
+                setThemeMode("light");
+              } else {
+                html?.classList.add("dark");
+                localStorage.setItem("themeMode", "dark");
+                setThemeMode("dark");
+              }
+            }}
+          >
+            {themeMode === "dark" ? (
+              <LucideMoon className="h-5 w-5" />
+            ) : (
+              <LucideSun className="h-5 w-5" />
+            )}
+            <span className="sr-only">Toggle Theme</span>
+          </Button>
+        </div>
       </div>
     </NavigationMenu>
   );
