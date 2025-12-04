f2a322844a9ca9b6f60be79ae53471375687acc8
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-12-04 00:08:42 -0500

    wip: page500

--- frontend/src/components/Page500.tsx
+++ frontend/src/components/Page500.tsx
@@ -6,14 +6,21 @@ import Zero from "./assets/Zero";
 import { useEffect, useRef } from "react";
 import Matter, { Runner } from "matter-js";
 
+const WALL_THICKNESS = 10;
+
 export default function Page500() {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!containerRef.current) return;
 
+    const { left, top, right, bottom, width, height } =
+      containerRef.current.getBoundingClientRect();
+
+    console.log({ left, top, right, bottom, width, height });
+
     const Engine = Matter.Engine;
-    const Render = Matter.Render;
+    const Render = Matter.Render; // for debugging
     const Bodies = Matter.Bodies;
     const Composite = Matter.Composite;
 
@@ -21,15 +28,25 @@ export default function Page500() {
     const render = Render.create({
       element: containerRef.current,
       engine: engine,
+      options: {
+        width,
+        height,
+        wireframes: false,
+        background: "transparent",
+      },
     });
 
     // bodies
-    const boxA = Bodies.rectangle(400, 200, 80, 80);
-    const boxB = Bodies.rectangle(450, 50, 80, 80);
-    const container = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    const boxB = Bodies.rectangle(425, 0, 80, 80);
+
+    // container
+    const leftWall = Bodies.rectangle(left, bottom, 10, height);
+    const floor = Bodies.rectangle(width / 2, height, width, WALL_THICKNESS, {
+      isStatic: true,
+    });
 
     // Add bodies to world
-    Composite.add(engine.world, [container, boxA, boxB]);
+    Composite.add(engine.world, [floor, leftWall, boxB]);
 
     // run renderer
     Render.run(render);
@@ -42,13 +59,13 @@ export default function Page500() {
   return (
     <div
       ref={containerRef}
-      className={`${neobrutalist()} h-[calc(100vh-195px)] mt-2`}
+      className={`${neobrutalist()} h-[calc(100vh-195px)] mt-2 relative`}
     >
-      <div className="flex items-center justify-center gap-2">
+      {/* <div className="flex items-center justify-center gap-2 absolute">
         <Five />
         <Zero />
         <Zero />
-      </div>
+      </div> */}
     </div>
   );
 }
