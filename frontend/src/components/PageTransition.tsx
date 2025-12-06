b186e80b4be8bbc4a281967d7a844fb404967b9b
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-12-06 08:29:02 -0500

    refactoring

--- frontend/src/components/PageTransition.tsx
+++ frontend/src/components/PageTransition.tsx
@@ -4,6 +4,8 @@ import * as motion from "motion/react-client";
 import { AnimatePresence } from "motion/react";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { dotBackgroundCn } from "./DotBackground";
+import { cn } from "@/lib/utils";
 
 export default function PageTransition({
   children,
@@ -28,7 +30,10 @@ export default function PageTransition({
           {new Array(3).fill(0).map((_, i) => (
             <motion.div
               key={`${pathname}-${i}`}
-              className={`h-screen w-1/3 z-100 fixed top-0 border-2 ${i === 0 ? "left-0" : i === 1 ? "left-1/3" : "left-2/3"} border-black dark:border-white bg-white bg-[radial-gradient(#c4c4c4_1px,transparent_1px)] bg-size-[20px_20px] dark:bg-black dark:bg-[radial-gradient(#3b3b3b_1px,transparent_1px)]`}
+              className={cn(
+                `h-screen w-1/3 z-100 fixed top-0 border-2 ${i === 0 ? "left-0" : i === 1 ? "left-1/3" : "left-2/3"}`,
+                dotBackgroundCn
+              )}
               animate={{ y: "-200vh" }}
               exit={{ y: 0 }}
               transition={{
