a89ea92ee0437e4f5df110c3ed1bd18ffd4696ea
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-12-05 20:54:07 -0500

    add page transition

--- frontend/src/components/PageTransition.tsx
+++ frontend/src/components/PageTransition.tsx
@@ -1,8 +1,10 @@
 "use client";
+
 import * as motion from "motion/react-client";
 import { AnimatePresence } from "motion/react";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { neobrutalist } from "@/lib/utils";
 
 export default function PageTransition({
   children,
@@ -16,13 +18,15 @@ export default function PageTransition({
       <AnimatePresence
         mode="wait"
         initial={false}
-        onExitComplete={() => setPath(pathname)}
+        onExitComplete={() => {
+          setPath(pathname);
+        }}
       >
         <motion.div key={pathname}>
           {new Array(4).fill(0).map((_, i) => (
             <motion.div
               key={`${pathname}-${i}`}
-              className={`h-screen w-1/4 z-100 bg-red-500 fixed top-0 ${i === 0 ? "left-0" : `left-${i}/4`}`}
+              className={`${neobrutalist()} h-screen w-1/4 z-10 fixed top-0 ${i === 0 ? "left-0" : `left-${i}/4`} bg-white bg-[radial-gradient(#c4c4c4_1px,transparent_1px)] bg-size-[20px_20px] dark:bg-black dark:bg-[radial-gradient(#3b3b3b_1px,transparent_1px)]`}
               animate={{ y: "-100%" }}
               exit={{ y: 0 }}
               transition={{
