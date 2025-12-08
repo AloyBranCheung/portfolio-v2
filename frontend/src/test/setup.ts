8cf1df9b27653af13268e0d9b70a0f88c3f38d03
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-12-07 19:01:01 -0500

    fix build

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,15 +1,18 @@
-import '@testing-library/jest-dom/vitest'
-import { cleanup } from '@testing-library/react'
-import { afterEach } from 'vitest'
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
 
+// @ts-expect-error - adding mock intersection observer for tests
 global.IntersectionObserver = class IntersectionObserver {
   constructor() {}
   disconnect() {}
   observe() {}
-  takeRecords() { return [] }
+  takeRecords() {
+    return [];
+  }
   unobserve() {}
-}
+};
