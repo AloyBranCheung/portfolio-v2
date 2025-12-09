a03ebb6972aa70c8f0b1fbb43b10e5341752a716
AloyBranCheung <brandon.cheung.dev@gmail.com>
2025-12-08 19:57:16 -0500

    fix typescript err

--- frontend/src/jotai-atoms/navbar.ts
+++ frontend/src/jotai-atoms/navbar.ts
@@ -3,7 +3,7 @@
 import { atom } from "jotai";
 
 // pathname
-export const isActiveAtom = atom(
+export const isActiveAtom = atom<string | null>(
   typeof window !== "undefined"
     ? window.location.pathname + window.location.hash
     : "/#about"
